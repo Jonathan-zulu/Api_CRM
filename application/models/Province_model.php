fc1762d32814d1084be9311f44c864df60fe9f99
Jonathan <jonathan30012rioja@gmail.com>
2024-03-22 11:01:33 +0100

    [Fix] Fixed class Province_model in Province_model.php

--- application/models/Province_model.php
+++ application/models/Province_model.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Province_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
 
     public function get_provinces() {
         $this->db->select('provincia_id, provincia_cp, provincia_nombre, provincia_capital, provincia_ca, provincia_autonomia');
