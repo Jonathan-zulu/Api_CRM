02ad4bda628bf95754e871611685e865c0aea010
Jonathan <jonathan30012rioja@gmail.com>
2024-03-22 10:43:50 +0100

    [Add] Added new model Province_model.php

---
+++ application/models/Province_model.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Province_model extends CI_Model {
+
+    public function get_provinces() {
+        $this->db->select('provincia_id, provincia_cp, provincia_nombre, provincia_capital, provincia_ca, provincia_autonomia');
+        $this->db->from('mahico_provincias');
+        $query = $this->db->get();
+
+        if ($query->num_rows() > 0) {
+            return $query->result_array();
+        } else {
+            return [];
+        }
+    }
+}
