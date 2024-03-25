57d6cd0c258b64bfc545bf5217d2e5890cdcefa3
Jonathan <jonathan30012rioja@gmail.com>
2024-03-25 09:34:55 +0100

    [Update] Updated register Feature

--- application/controllers/Technicians_api.php
+++ application/controllers/Technicians_api.php
@@ -81,6 +81,14 @@ class Technicians_api extends CI_Controller {
     }
 
     public function register() {
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+            header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+            exit;
+        }
+
         header('Access-Control-Allow-Origin: *');
 
         header('Content-Type: application/json');
